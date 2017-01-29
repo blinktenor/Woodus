638fbd5838cc4503e825f2b529cab52470ad8523
Bob Moore <blinktenor@gmail.com>
2017-01-29 17:54:39 -0500

    Same

--- public/core.js
+++ public/core.js
@@ -1,150 +1,35 @@
-var Woodus = angular.module('Woodus', []);
+angular.module('Woodus', [])
+//.config(function($mdIconProvider) {
+//    $mdIconProvider
+//      .iconSet("call", 'img/icons/sets/communication-icons.svg', 24)
+//      .iconSet("social", 'img/icons/sets/social-icons.svg', 24);
+//  })
+.controller('mainController',
+function ($scope) {
 
-function mainController($scope, $http) {
+    $scope.options = [{creature : "Dracolord (Human Form)", sources : [{"Servant" : "Andreal"}, {"Servant" : "GreatDrak"}]},
+{creature : "Dracolord (Dragon)", sources : [{"DracoLord (Human Form)" : "Divinegon"}]},
+{creature : "LordDraco", sources : [{"DracoLord (Dragon)" : "Orligon"}]},
+{creature : "Hargon", sources : [{"WhiteKing" : "MetalKing"}]},
+{creature : "Sidoh", sources : [{"Jamirus" : "Rosevine"}]},
+{creature : "Genosidoh", sources : [{"Sidoh" : "Titanis"}]},
+{creature : "Baramos", sources : [{"Hargon" : "Orochi"}]},
+{creature : "Zoma", sources : [{"DracoLord (Human Form)" : "Sidoh"},{"DracoLord (Dragon)" : "Sidoh"}]},
+{creature : "Asurazoma", sources : [{"Zoma" : "PsychoPiz"}]},
+{creature : "Pizzaro", sources : [{"Durran" : "Divinegon"}]},
+{creature : "Psychopiz", sources : [{"Pizzaro" : "Esterk"}]},
+{creature : "Esterk", sources : [{"Pizzaro" : "Gorago"},{"Pizzaro" : "Kingleo"}]},
+{creature : "Mirudraas", sources : [{"Esterk" : "GoldSlime"}]},
+{creature : "Mirudraas (Transformed)", sources : [{"Mirudraas" : "Orligon"}]},
+{creature : "Mudou", sources : [{"Baramos" : "Gorago"}]},
+{creature : "Deathmore", sources : [{"Mirudraas" : "Azurile"}, {"Mirudraas (Transformed)" : "Zoma"}, {"Zoma" : "Mirudraas"}, {"Zoma" : "Mirudraas (Transformed)"},{"Zoma" : "Azurile"}]},
+{creature : "Deathmore (Transformed)", sources : [{"DeathMore" : "Titanis"},{"DeathMore" : "Armorpion"}]},
+{creature : "Deathmore (Final Form)", sources : [{"Deathmore (Transformed)" : "DarkMate"},{"Deathmore (Transformed)" : "Mudou"},{"Deathmore (Transformed)" : "Poseidon"}]},
+{creature : "Darkdrium", sources : [{"Deathmore (Final Form)" : "GranSlime"},{"Deathmore (Final Form)" : "AsuraZoma"},{"Deathmore (Transformed)" : "Watabou"}]},
+{creature : "Orgodemir", sources : [{"PsychoPiz" : "Niterich"}]},
+{creature : "Orgodemir (Transformed)", sources : [{"Orgodemir" : "LordDraco"}]}];
 
-//    $scope.monsterData = [{creature: "Dracolord (Human Form)", sources: [{"Servant": ["Andreal", "GreatDrak"]}]},
-//        {creature: "Rayburn", sources: [{"LizardMan":["WingSlime","Trumpeter","MadCat","FaceTree"]},{"Any Dragon":["MadCondor"]}]}];
-//    ,
-//        {creature: "Dracolord (Dragon)", sources: [{"DracoLord (Human Form)": "Divinegon"}]},
-//        {creature: "LordDraco", sources: [{"DracoLord (Dragon)": "Orligon"}]},
-//        {creature: "Hargon", sources: [{"WhiteKing": "MetalKing"}]},
-//        {creature: "Sidoh", sources: [{"Jamirus": "Rosevine"}]},
-//        {creature: "Genosidoh", sources: [{"Sidoh": "Titanis"}]},
-//        {creature: "Baramos", sources: [{"Hargon": "Orochi"}]},
-//        {creature: "Zoma", sources: [{"DracoLord (Human Form)": "Sidoh"}, {"DracoLord (Dragon)": "Sidoh"}]},
-//        {creature: "Asurazoma", sources: [{"Zoma": "PsychoPiz"}]},
-//        {creature: "Pizzaro", sources: [{"Durran": "Divinegon"}]},
-//        {creature: "Psychopiz", sources: [{"Pizzaro": "Esterk"}]},
-//        {creature: "Esterk", sources: [{"Pizzaro": "Gorago"}, {"Pizzaro": "Kingleo"}]},
-//        {creature: "Mirudraas", sources: [{"Esterk": "GoldSlime"}]},
-//        {creature: "Mirudraas (Transformed)", sources: [{"Mirudraas": "Orligon"}]},
-//        {creature: "Mudou", sources: [{"Baramos": "Gorago"}]},
-//        {creature: "Deathmore", sources: [{"Mirudraas": "Azurile"}, {"Mirudraas (Transformed)": "Zoma"}, {"Zoma": "Mirudraas"}, {"Zoma": "Mirudraas (Transformed)"}, {"Zoma": "Azurile"}]},
-//        {creature: "Deathmore (Transformed)", sources: [{"DeathMore": "Titanis"}, {"DeathMore": "Armorpion"}]},
-//        {creature: "Deathmore (Final Form)", sources: [{"Deathmore (Transformed)": "DarkMate"}, {"Deathmore (Transformed)": "Mudou"}, {"Deathmore (Transformed)": "Poseidon"}]},
-//        {creature: "Darkdrium", sources: [{"Deathmore (Final Form)": "GranSlime"}, {"Deathmore (Final Form)": "AsuraZoma"}, {"Deathmore (Transformed)": "Watabou"}]},
-//        {creature: "Orgodemir", sources: [{"PsychoPiz": "Niterich"}]},
-//        {creature: "Orgodemir (Transformed)", sources: [{"Orgodemir": "LordDraco"}]}];
-
-    $scope.currentTree = {"0":
-                [
-                    {
-                        "level": 0,
-                        "pos": 0,
-                        "name": "DracoLord (Human)",
-                        "offspring": -1,
-                        "parents": [[1, 0], [1, 1]],
-                        "found": false,
-                        "options": []
-                    }
-                ],
-        "1":
-                [
-                    {
-                        "level": 1,
-                        "pos": 0,
-                        "name": "Servant",
-                        "offspring": [1, 1],
-                        "parents": [[2, 0], [2, 1]],
-                        "found": false,
-                        "options": []
-                    },
-                    {
-                        "level": 1,
-                        "pos": 1,
-                        "name": "Great Drak",
-                        "offspring": [1, 1],
-                        "parents": [],
-                        "found": true,
-                        "options": []
-                    }
-                ],
-        "2":
-                [
-                    {
-                        "level": 2,
-                        "pos": 0,
-                        "name": "Skeletor",
-                        "offspring": [2, 1],
-                        "parents": [],
-                        "found": true,
-                        "options": []
-                    },
-                    {
-                        "level": 2,
-                        "pos": 1,
-                        "name": "Skeletor",
-                        "offspring": [2, 1],
-                        "parents": [[3, 2], [3, 3]],
-                        "found": false,
-                        "options": []
-                    },
-                    {},
-                    {}
-                ],
-        "3":
-                [
-                    {
-                        "options": ["boneslave", "your mom"],
-                        "optionData":[
-                            {
-                                "primary": "boneslave",
-                                "mate": ["boneslave"]
-                            },
-                            {
-                                "primary": "your mom",
-                                "mate": ["your other mom", "your dad"]
-                            }
-                        ]
-                    },
-                    {},
-                    {
-                        "level": 3,
-                        "pos": 2,
-                        "name": "Boneslave",
-                        "offspring": [3, 2],
-                        "parents": [],
-                        "found": true,
-                        "options": []
-                    },
-                    {
-                        "level": 3,
-                        "pos": 3,
-                        "name": "Boneslave",
-                        "offspring": [3, 2],
-                        "parents": [],
-                        "found": true,
-                        "options": []
-                    },
-                    {},
-                    {},
-                    {},
-                    {}
-                ]
-    };
-
-    $scope.selected;
-
-    $scope.Math = window.Math;
-
-    $scope.$watch('selected', function (oldValue, newValue) {
-    });
-
-    $scope.foundCreature = function (creature) {
-        $scope.currentTree[creature.level][creature.pos].found = !creature.found;
-        if (!creature.found)
-        {
-            console.log("already found, get rid of parents!");
-            //get the parents
-            //remove them and set them as {}
-            removeParentsAndDown(creature);
-            $scope.currentTree[creature.level][creature.pos].parents = [];
-        } else
-        {
-            console.log("Not found, here are my parents!");
-            //add selector for parents
-        }
-    };
+    $scope.creatureCache = {};
 
     function removeParentsAndDown(creature) {
         var parents = creature.parents;
@@ -153,4 +38,71 @@ function mainController($scope, $http) {
             $scope.currentTree[parents[a][0]][creature.parents[a][1]] = {};
         }
     }
+    
+    var totalCritters = 1;
+    $scope.desiredCreature = [new Creature("Dracolord (Human Form)", -1, 0, 0)];
+    
+    function Creature(name, offspring) {
+        this.name = name;
+        this.offspring = offspring;
+        this.found = false;
+        this.options = getCreatureOptions(name);
+        this.parents = [];
+        this.addParent = function(parentName) {
+          this.parents.push(new Creature(parentName, this));
+        };
+        this.bred = function () {
+            if (this.found) {
+                this.found = false;
+            } else {
+                this.parents = [];
+                this.found = true;
+            }
+        };
+    }
+    
+    function getCreatureOptions(creatureName) {
+        if (!$scope.creatureCache[creatureName]) $scope.creatureCache[creatureName] = lookupCreature(creatureName);
+        return $scope.creatureCache[creatureName];
+    }
+    
+    function lookupCreature(creatureName) {
+        for(var index in $scope.options) {
+            if ($scope.options[index].creature === creatureName) {
+                return $scope.options[index].sources;
+            }
+        }
+    }
+    
+    $scope.addLeaf = function () {
+        var critter = getRandomCritter();
+        critter.addParent("Servant");
+        totalCritters++;
+    };
+    
+    function getRandomCritter() {
+        var selected = Math.floor(Math.random() * (totalCritters - 1)) + 1;
+        return getCritter($scope.desiredCreature[0], selected);
+    }
+    
+    function getCritter(creature, selected) {
+        if (selected === 1) {
+            return creature;
+        }
+        for(var a = 0; a < creature.parents.length; a++) {
+            selected--;
+            var crit = getCritter(creature.parents[a], selected);
+            if (typeof(crit) === 'object') {
+                return crit;
+            } else {
+                selected = crit;
+            }
+        }
+        return selected;
+    }
+    
+    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+            'WY').split(' ').map(function (state) { return { abbrev: state }; });
 }
+);
